e879886ac565f5780d310ecfc5e78e3851ddc01a
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-15 21:27:37 +0530

    [Ajay] Welcome to FlipCoin Program

--- BasicLogicalProgram/Program.cs
+++ BasicLogicalProgram/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Logical Program");
+            Console.WriteLine("1:Flip coin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoinUC1 flipCoin = new FlipCoinUC1();
+                    flipCoin.FlipCoins();
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
         }
     }
 }
